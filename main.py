0d6e39a3d701aeb5f60dbfba08450a6440b3fe90
diederick <diederickc@gmail.com>
2024-03-24 14:35:44 +0200

    added print title and description

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 
 api_key = "152f7e7e6a4047bdbcbd14f6734f2349"
 
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-02-24&sortBy=publishedAt&apiKey=152f7e7e6a4047bdbcbd14f6734f2349")
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-02-24&sortBy=publishedAt&apiKey\
+=152f7e7e6a4047bdbcbd14f6734f2349")
 
 # Make request
 request = requests.get(url)
